7863bb02d91fa08a277920206ce161a82357aaa2
Bala Krish <56611791+sbkrishnan2506@users.noreply.github.com>
2023-05-30 16:06:39 -0500

    Add files via upload
    Initial commit

---
+++ MauiAppPreference/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppPreference;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiAppPreference/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiAppPreference;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		GetPreference();
+    }
+
+    private void GetPreference()
+    {
+        var val = Preferences.Default.Get("first_name", string.Empty); 
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
